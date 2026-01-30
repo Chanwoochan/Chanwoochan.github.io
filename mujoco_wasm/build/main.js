08db895e5f09641276817d80c1c37102f1dae919
Chanwoochan <kcwjoma@kookmin.ac.kr>
2026-01-30 20:42:49 +0900

    .

--- mujoco_wasm/build/main.js
+++ mujoco_wasm/build/main.js
@@ -30879,16 +30879,16 @@ function setupGUI(parentContext) {
   });
   let reload = reloadFunc.bind(parentContext);
   parentContext.gui.add(parentContext.params, "scene", {
-    "DARU": "humanoid.xml"
-    // "Cassie": "agility_cassie/scene.xml",
-    // "Hammock": "hammock.xml",
-    // "Balloons": "balloons.xml",
-    // "Hand": "shadow_hand/scene_right.xml",
-    // "Mug": "mug.xml",
-    // "Tendon": "model_with_tendon.xml",
-    // "Torture Model": "model.xml",
-    // "Flex": "flex.xml",
-    // "Car": "car.xml"
+    "Humanoid": "humanoid.xml",
+    "Cassie": "agility_cassie/scene.xml",
+    "Hammock": "hammock.xml",
+    "Balloons": "balloons.xml",
+    "Hand": "shadow_hand/scene_right.xml",
+    "Mug": "mug.xml",
+    "Tendon": "model_with_tendon.xml",
+    "Torture Model": "model.xml",
+    "Flex": "flex.xml",
+    "Car": "car.xml"
   }).name("Example Scene").onChange(reload);
   let keyInnerHTML = "";
   let actionInnerHTML = "";
